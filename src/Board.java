8a097c591207bbbb6a5e27edac0bc1039d7af5b4
Vaughn Bosu <vwbosu@gmail.com>
2023-08-31 12:24:54 -0700

    8/31/23 (day 5)

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Board {
 
     public String[][] board = new String[9][9];
@@ -5,7 +7,7 @@ public class Board {
     public String[][] setupBoard() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                board[i][j] = "-";
+                board[i][j] = " \uD83C\uDF0A ";
             }
         }
         return board;
--- src/Ship.java
+++ src/Ship.java
@@ -1,3 +1,4 @@
+
 public class Ship {
 
     private int length;
