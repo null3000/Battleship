d322175fb37004eeb9edcc72f585b91cc27b230e
Vaughn Bosu <vwbosu@gmail.com>
2023-09-01 15:12:25 -0700

    9/1/23 (day 6)

--- src/Board.java
+++ src/Board.java
@@ -33,9 +33,38 @@ public class Board {
                 board[xPos + i][yPos] = " \uD83D\uDEA2 ";
             }
         }
+    }
 
+    public boolean checkValidPlacement(int OrientationChoice, int length, int xPos, int yPos) {
 
+        boolean valid = true;
+//        check if a ship is already in the location
+        if (OrientationChoice == 1) {
+            for (int i = 0; i < length; i++) {
+                if (board[xPos][yPos + i].equals(" \uD83D\uDEA2 ")) {
+                    valid = false;
+                }
+            }
+        } else if (OrientationChoice == 2)  {
+            for (int i = 0; i < length; i++) {
+                if (board[xPos + i][yPos].equals(" \uD83D\uDEA2 ")) {
+                    valid = false;
+                }
+            }
+        }
 
+//        check if the ship is out of bounds on the Board
+        if(OrientationChoice == 1) {
+            if(xPos + length > 9) {
+                valid = false;
+            }
+        } else if(OrientationChoice == 2) {
+            if(yPos + length > 9) {
+                valid = false;
+            }
+        }
+
+        return valid;
 
     }
 
--- src/Player.java
+++ src/Player.java
@@ -34,44 +34,55 @@ public class Player {
 
 		public void placeShips(){
 				setupPossibleLengths();
-				System.out.println(name + " place your ships");
-				System.out.println("How would you like to orient your ship");
-				System.out.println("1. Horizontal");
-				System.out.println("2. Vertical");
-				Scanner scanner = new Scanner(System.in);
-				int choice = scanner.nextInt();
-				while(choice!=1 && choice!=2){
-					System.out.println("Please enter a valid choice");
-					choice = scanner.nextInt();
+				while(possibleLengths.size() > 0) {
+						System.out.println(name + " place your ships");
+						System.out.println("How would you like to orient your ship");
+						System.out.println("1. Horizontal");
+						System.out.println("2. Vertical");
+						// Take their input for orientation AND validate it is only 1 or 2, if 1 set it horizontal if 2 set it vertical
+						Scanner scanner = new Scanner(System.in);
+						int choice = scanner.nextInt();
+						while (choice != 1 && choice != 2) {
+								System.out.println("Please enter a valid choice");
+								choice = scanner.nextInt();
+						}
+
+						// Print the values for lengths and take user input for what length ship they want to place
+						printPossibleLengths();
+						int length = 0;
+						System.out.println("What length do you choose?");
+						length = scanner.nextInt();
+						// Check if length is equal to one of the values in possible lengths
+						while (!possibleLengths.contains(length)) {
+								System.out.println("Please enter a valid length");
+								length = scanner.nextInt();
+						}
+						// Remove already used lengths from the possibleLengths ArrayList
+						possibleLengths.remove(possibleLengths.indexOf(length));
+
+						// Take inputs for where user wants for X and Y coordinates of ship
+						System.out.println("What is the x position of your ship?");
+						int xPos = scanner.nextInt();
+						System.out.println("What is the y position of your ship?");
+						int yPos = scanner.nextInt();
+						// Check if the placement is valid
+						while (!board.checkValidPlacement(choice, length, xPos, yPos)){
+								System.out.println("Please enter a valid placement");
+								System.out.println("What is the x position of your ship?");
+								xPos = scanner.nextInt();
+								System.out.println("What is the y position of your ship?");
+								yPos = scanner.nextInt();
+						}
+						if (choice == 1) {
+								Ship ship = new Ship(length, xPos, yPos, "horizontal");
+								ships.add(ship);
+								board.placeShip("horizontal", length, xPos, yPos);
+						} else if (choice == 2) {
+								Ship ship = new Ship(length, xPos, yPos, "vertical");
+								ships.add(ship);
+								board.placeShip("vertical", length, xPos, yPos);
+						}
 				}
-				printPossibleLengths();
-				int length = 0;
-//				check if length is equal to one of the values in possible lengths
-				while(!possibleLengths.contains(length)){
-					System.out.println("Please enter a valid length");
-					length = scanner.nextInt();
-				}
-				System.out.println("What length do you choose?");
-				length = scanner.nextInt();
-
-				System.out.println("What is the x position of your ship?");
-				int xPos = scanner.nextInt();
-				System.out.println("What is the y position of your ship?");
-				int yPos = scanner.nextInt();
-
-				if(choice == 1) {
-						Ship ship = new Ship(length, xPos, yPos, "horizontal");
-						ships.add(ship);
-						board.placeShip("horizontal", length, xPos, yPos);
-				} else if(choice == 2) {
-						Ship ship = new Ship(length, xPos, yPos, "vertical");
-						ships.add(ship);
-						board.placeShip("vertical", length, xPos, yPos);
-				}
-
-
-
-
 		}
 
 		public void setupPossibleLengths() {
